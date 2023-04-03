61f3899b9744eba674b892d332f8719fde0c52e7
pytest <pytest514@gmail.com>
2023-04-04 02:16:47 +0800

    feat: add cart context provider wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyles from "./GlobalStyles/GlobalStyles";
 import "./index.css";
+import CartContextProvider from "./store/cart-context";
 
 const rootEle = ReactDOM.createRoot(document.getElementById("root"));
 
 rootEle.render(
   <React.StrictMode>
-    <App />
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
     <GlobalStyles />
   </React.StrictMode>
 );
