20d331cccc516a507c555727d79e01cc08aa0d1e
pytest <pytest514@gmail.com>
2023-04-03 21:55:12 +0800

    feat: add basic html and data fetching

--- src/pages/ProducetDetails.jsx
+++ src/pages/ProducetDetails.jsx
@@ -1,7 +1,42 @@
 import styled from "styled-components";
+import Button from "../ui/Button";
+import COOKIES_DATA from "../data/CookiesData";
+import { useParams } from "react-router-dom";
 
 export default function ProductDetails() {
-  return <Wrapper></Wrapper>;
+  const formSubmitHandler = () => {};
+  const { productId } = useParams();
+
+  const { name, price, description, url } = COOKIES_DATA.find((i) => {
+    return i.id === +productId;
+  });
+
+  return (
+    <Wrapper>
+      <ProductImage src={url}></ProductImage>
+      <OrderForm onSubmit={formSubmitHandler}>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+        <Description>{description}</Description>
+        <QuantityLabel htmlFor="quantity">Quantity</QuantityLabel>
+        <ProductQuantity
+          id="quantity"
+          name="quantity"
+          type="number"
+          min="0"
+        ></ProductQuantity>
+        <AddToCartButton>Add To Cart</AddToCartButton>
+      </OrderForm>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div``;
+const ProductImage = styled.img``;
+const Name = styled.h1``;
+const Price = styled.div``;
+const Description = styled.div``;
+const OrderForm = styled.form``;
+const QuantityLabel = styled.label``;
+const ProductQuantity = styled.input``;
+const AddToCartButton = styled(Button)``;
