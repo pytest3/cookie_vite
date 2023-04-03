b6f0494d11b06134fb28c8402ddf15186e0abc22
pytest <pytest514@gmail.com>
2023-04-04 01:50:36 +0800

    feat: create cart context provider

---
+++ src/store/cart-context.jsx
@@ -0,0 +1,43 @@
+import { useState, createContext } from "react";
+
+export const CartContext = createContext({ showCart: false, products: [] });
+
+export default function CartContextProvider({ children }) {
+  const [products, setProducts] = useState(INITIAL_PRODUCTS);
+
+  function addProduct(product) {
+    return setProducts([...products, product]);
+  }
+
+  function removeProduct(productId) {
+    return products.filter((i) => i.id !== productId);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        showCart: false,
+        products: products,
+        addProduct: addProduct,
+        removeProduct: removeProduct,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+const INITIAL_PRODUCTS = [
+  {
+    id: 1,
+    name: "Cookie1",
+    price: 2,
+    url: "/images/double-choc-chip-cookie.png",
+  },
+  {
+    id: 2,
+    name: "Cookie2",
+    price: 4,
+    url: "/images/double-choc-chip-cookie.png",
+  },
+];
