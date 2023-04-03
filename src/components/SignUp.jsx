9c0cce5228086ee631ffa24a10a79df78f5b6132
pytest <pytest514@gmail.com>
2023-04-03 18:32:33 +0800

    feat: add sign up component

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import Button from "../ui/Button";
+import { useState, useEffect } from "react";
+
+export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState();
+  const [password, setPassword] = useState("");
+  const [passwordValid, setPasswordValid] = useState();
+
+  useEffect(() => {}, [password]);
+
+  const emailChangeHandler = (e) => {
+    setEmail(e.target.value);
+    setEmailValid(email.includes("@") && email.trim().length > 4);
+  };
+
+  const validateEmail = () => {
+    setEmailValid(email.includes("@") && email.trim().length > 4);
+  };
+
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+    setPasswordValid(password.trim().length > 3);
+  };
+
+  const validatePassword = () => {
+    setPasswordValid(password.trim().length > 3);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    alert("lol");
+  };
+
+  const validForm = passwordValid & emailValid;
+
+  return (
+    <Wrapper>
+      Join us to receive 10% off your first order.
+      <Form onSubmit={formSubmitHandler}>
+        <EmailLabel htmlFor="email">Email</EmailLabel>
+        <EmailInput
+          type="email"
+          id="email"
+          name="email"
+          onChange={emailChangeHandler}
+          onBlur={validateEmail}
+          isValid={emailValid}
+        ></EmailInput>
+        <PasswordLabel htmlFor="password">Password</PasswordLabel>
+        <PasswordInput
+          type="password"
+          id="email"
+          name="email"
+          onChange={passwordChangeHandler}
+          onBlur={validatePassword}
+          isValid={passwordValid}
+        ></PasswordInput>
+        <SubmitButton disabled={!validForm}>Submit</SubmitButton>
+      </Form>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  padding: 24px 0px;
+  border: 1px solid var(--color-gray-300);
+`;
+
+const Form = styled.form`
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+  justify-content: start;
+`;
+
+const EmailLabel = styled.label``;
+
+const EmailInput = styled.input`
+  background-color: ${(p) =>
+    p.isValid === false ? "var(--color-urgent)" : "inherit"};
+`;
+
+const PasswordLabel = styled(EmailLabel)``;
+
+const PasswordInput = styled(EmailInput)``;
+
+const SubmitButton = styled(Button)`
+  background-color: ${(p) => p.disabled && "var(--color-gray-300)"};
+  color: ${(p) => p.disabled && "var(--color-gray-700)"};
+`;
