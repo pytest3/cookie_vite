43148604f0cfbe2e4908a0564d30610dadc503dc
pytest <pytest514@gmail.com>
2023-04-06 10:58:04 +0800

    style: restyle join us segment

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -35,8 +35,8 @@ export default function SignUp() {
 
   return (
     <Wrapper>
-      Join us to receive 10% off your first order.
       <Form onSubmit={formSubmitHandler}>
+        <FormHeader>Join us for 10% off</FormHeader>
         <EmailLabel htmlFor="email">Email</EmailLabel>
         <EmailInput
           type="email"
@@ -65,37 +65,63 @@ export default function SignUp() {
         )}
         <SubmitButton disabled={!validForm}>Submit</SubmitButton>
       </Form>
+      <OkSignImage>
+        <img src="/images/OK-sign-improved.png" alt="hand with cookie"></img>
+      </OkSignImage>
     </Wrapper>
   );
 }
+
+const OkSignImage = styled.div`
+  width: 200px;
+  margin: -38px 0px;
+`;
+const FormHeader = styled.h1`
+  /* font-size: clamp(2.5rem, 4vw, 3rem); */
+  font-size: clamp(0.8rem, 0.3vw + 1rem, 2rem);
+  color: var(--color-white);
+`;
 
 const ValidationMessage = styled.div`
   color: var(--color-gray-300);
 `;
 
 const Wrapper = styled.div`
-  padding: 24px 0px;
+  padding: 38px 0px;
   border: 1px solid var(--color-gray-300);
+  /* background-color: var(--color-creamV1); */
+  background-color: var(--color-CTA);
+  margin-left: -30px;
+  margin-right: -30px;
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  gap: 38px;
 `;
 
 const Form = styled.form`
   display: grid;
   grid-template-rows: repeat(3, auto);
   justify-content: start;
+  gap: 4px;
+  color: var(--color-white);
 `;
 
 const EmailLabel = styled.label``;
 
 const EmailInput = styled.input`
   background-color: ${(p) =>
-    p.isValid === false ? "var(--color-urgent)" : "inherit"};
+    p.isValid === false ? "var(--color-urgent)" : "var(--color-white)"};
 `;
 
 const PasswordLabel = styled(EmailLabel)``;
 
-const PasswordInput = styled(EmailInput)``;
+const PasswordInput = styled(EmailInput)`
+  background-color: var(--color-white);
+`;
 
 const SubmitButton = styled(Button)`
   background-color: ${(p) => p.disabled && "var(--color-gray-300)"};
   color: ${(p) => p.disabled && "var(--color-gray-700)"};
+  margin-top: 10px;
 `;
